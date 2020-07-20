758f1f600183a668c489edc7316a06e8f8cd80f6
Arina <byunarina@gmail.com>
2020-07-20 13:50:25 +0300

    chess: new try

---
+++ chess_2.0/chess_2.0.js
@@ -0,0 +1,14 @@
+for (var i = 0; i <= 7; i++) {
+    var row = document.createElement("div");
+    row.setAttribute("class", "row");
+
+    for (var j = 1; j <= 8; j++) {
+        var cell = document.createElement("div");
+        cell.setAttribute("class", "cell");
+        if ((j+i)%2 === 1) {
+            cell.setAttribute("class", "cell cell_black");
+        }
+        row.appendChild(cell);
+    }
+    document.getElementById("main").appendChild(row);
+}
